3282e234ae29c8b10c983108861522b9c8f4c14f
V E L Δ <36549174+VELD-Dev@users.noreply.github.com>
2025-05-13 21:11:48 +0200

    Refactoring

---
+++ ratchet1/boot.cpp
@@ -0,0 +1,39 @@
+﻿#include "boot.h"
+#include "../ProjectLuna/common.h"
+
+#include <cstdio>
+
+int main(int argc, char* argv[])
+{
+    current_level = VELDIN_INTRO;
+
+    DBG("Loading Ratchet & Clank (2002)...\n");
+    DBG("Current level: %d", current_level);
+
+    // Startup
+    
+    // Init game graphics
+
+    // Check saves
+
+    bool shouldClose = false;
+
+    long long int frameCount = 0;
+
+    // Game loop
+    while (!shouldClose)
+    {
+        _sleep(1);
+        frameCount++;
+
+        if (frameCount > 1000)
+            shouldClose = true;
+    }
+    
+    return 0;
+}
+
+void start()
+{
+    
+}
---
+++ ratchet1/boot.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+extern int current_level;
+
+constexpr int VELDIN_INTRO = -1;
+constexpr int VELDIN_START = 0;
+constexpr int NOVALIS = 1;
+
+int main(int argc, char* argv[]);
